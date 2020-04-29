a915274c948c95cd1bc989591cbce04c3a876deb
Emery <emery@iubang.com>
2020-04-29 09:40:20 +0800

    update ice server and coturn deploy script

--- coturn.sh
+++ coturn.sh
@@ -1,3 +1,4 @@
-docker run -d --name coturn -p 3478:3478 -p 3478:3478/udp -p 49160-49200:49160-49200/udp \
+docker run -d --name coturn -p 3478:3478 -p 3478:3478/udp -p 49160-49200:49160-49200/udp -v /srv/coturn/data:/var/lib/coturn\
     instrumentisto/coturn:4.5.1.1 -n --log-file=stdout \
+    --external-ip='139.196.242.41' \
     --min-port=49160 --max-port=49200
